bbae8e3dedc5e5616c18cc9eeffec5edf1e24595
akinoneko <zhangyuyang@live.cn>
2014-05-10 16:33:33 +0800

    更新详情见描述
    新增：
    1. 新增了创建班级时对人员的编辑功能
    更新：
    1. 更新创建班级的流程
    修复：
    1. 修复了重复班级检测模块由于逻辑问题产生的重复检测失效

--- src/org/attendance/servlet/CreateClassServlet.java
+++ src/org/attendance/servlet/CreateClassServlet.java
@@ -1,11 +1,21 @@
 package org.attendance.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import cn.hyit.zyy.dao.factory.DAOFactory;
+import cn.hyit.zyy.vo.CourseInfo;
+import cn.hyit.zyy.vo.GroupInfo;
+import cn.hyit.zyy.vo.SubjectInfo;
 
 /**
  * Servlet implementation class CreateClass
@@ -29,7 +39,65 @@ public class CreateClassServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-
+		String[] studentid = request.getParameterValues("studentid");
+		String classname = request.getParameterValues("classname")[0];
+		String subjectname = request.getParameterValues("subject")[0];
+		int teacherid = (int) request.getSession().getAttribute("uid");
+		/**
+		 * 开始班级的创建
+		 */
+		SubjectInfo subject = new SubjectInfo();
+		subject.setSubjectname(subjectname);
+		try {
+			DAOFactory.getISubjectInfoDAOInstance().doCreate(subject);
+			subject = DAOFactory.getISubjectInfoDAOInstance().findByName(
+					subject.getSubjectname());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		CourseInfo course = new CourseInfo();
+		GroupInfo group = new GroupInfo();
+		course.setSubjectid(subject.getSubjectid());
+		course.setTeacherid(teacherid);
+		try {
+			DAOFactory.getICourseInfoDAOInstance().doCreate(course);
+			course = DAOFactory.getICourseInfoDAOInstance().findGroup(
+					course.getTeacherid(), course.getSubjectid());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		group.setGroupid(course.getGroupid());
+		group.setGroupname(classname);
+		for (String id : studentid) {
+			group.setStudentid(Integer.parseInt(id));
+			try {
+				DAOFactory.getIGroupInfoDAOInstance().doCreate(group);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		/**
+		 * 班级创建结束 重新获取教师的课程列表
+		 */
+		List<Integer> allSubjectid = null;
+		Iterator<Integer> iterSubid = null;
+		List<String> allSubjectName = new ArrayList<>();
+		try {
+			allSubjectid = DAOFactory.getICourseInfoDAOInstance().findById(
+					teacherid);
+			iterSubid = allSubjectid.iterator();
+			while (iterSubid.hasNext()) {
+				subject = DAOFactory.getISubjectInfoDAOInstance().findById(
+						iterSubid.next().intValue());
+				allSubjectName.add(subject.getSubjectname());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		request.getSession().setAttribute("allSubjectid", allSubjectid);
+		request.getSession().setAttribute("allSubjectName", allSubjectName);
+		RequestDispatcher rd = request.getRequestDispatcher("queryClassList");
+		rd.forward(request, response);
 	}
 
 	/**
